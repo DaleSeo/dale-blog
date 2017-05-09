82902b15d6114e51e8d2d684400b74a482a776a1
Dale Seo <dale.seo@gmail.com>
2017-05-09 22:41:11 +0900

    user router

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,62 @@
-var express = require('express');
-var router = express.Router();
+const router = require('express').Router();
+const userSvc = require('../services/userSvc')
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+router.get('/', (req, res) => {
+  userSvc.list()
+    .then(users => {
+      console.log(users)
+      return users
+    })
+    .then(users => {
+      res.render('index', {users: users})
+    })
+})
+
+const newUser = {
+  'name': '',
+  'role': 'Guest',
+  'email': '',
+  'score': 0,
+  'img': 'guest.png'
+}
+
+router.get('/add', (req, res) => {
+  res.render('edit', {user: newUser})
+})
+
+router.post('/add', (req, res) => {
+  userSvc.create(req.body)
+    .then(id => {
+      res.redirect(id)
+    })
+})
+
+router.get('/:id', (req, res) => {
+  userSvc.detail(req.params.id)
+    .then(user => {
+      res.render('view', {user: user})
+    })
+})
+
+router.get('/:id/del', (req, res) => {
+  userSvc.remove(req.params.id)
+    then(_ => {
+      res.redirect('/users')
+    })
+})
+
+router.get('/:id/edit', (req, res) => {
+  userSvc.detail(req.params.id)
+    .then(user => {
+      res.render('edit', {user: user})
+    })
+})
+
+router.post('/:id/edit', (req, res) => {
+  userSvc.modify(req.params.id, req.body)
+    .then(_ => {
+      res.redirect('/users/' + req.params.id)
+    })
+})
 
 module.exports = router;
---
+++ services/userSvc.js
@@ -0,0 +1,34 @@
+const MongoClient = require('mongodb').MongoClient
+const ObjectID = require('mongodb').ObjectID
+const url = 'mongodb://localhost:27017/ltcs'
+
+exports.list = function () {
+  return MongoClient.connect(url)
+    .then(db => db.collection('users'))
+    .then(collection => collection.find({}).toArray())
+}
+
+exports.detail = function (id) {
+  return MongoClient.connect(url)
+    .then(db => db.collection('users'))
+    .then(collection => collection.findOne({_id: new ObjectID(id)}))
+}
+
+exports.create = function (user) {
+  return MongoClient.connect(url)
+    .then(db => db.collection('users'))
+    .then(collection => collection.insertOne(user))
+    .then(res => res.ops[0]._id)
+}
+
+exports.modify = function (id, user) {
+  return MongoClient.connect(url)
+    .then(db => db.collection('users'))
+    .then(collection => collection.updateOne({_id: new ObjectID(id)}, {$set: user}))
+}
+
+exports.remove = function (id) {
+  return MongoClient.connect(url)
+    .then(db => db.collection('users'))
+    .then(collection => collection.removeOne({_id: new ObjectID(id)}))
+}
