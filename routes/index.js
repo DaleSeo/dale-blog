9a96b413eeb4df0a482cfb30d68a744df936a8bb
Dale Seo <dale.seo@gmail.com>
2017-05-17 19:44:38 +0900

    admin page

--- routes/index.js
+++ routes/index.js
@@ -3,9 +3,20 @@ const Article = require('../models/article')
 
 router.get('/', (req, res) => {
   Article.list().byPublished(true)
-    .then(articles =>
-      res.render('index', {articles: articles}))
+    .then(articles => res.render('index', {articles: articles}))
 })
+
+router.get('/admin', (req, res) => {
+  Article.list()
+    .then(articles => res.render('admin', {articles}))
+})
+
+exports.admin = function (req, res, next) {
+  req.models.Article.list((error, articles) => {
+    if (error) return next(error)
+    res.render('admin', {articles: articles})
+  })
+}
 
 router.get('/test', (req, res) => {
   res.send('<h1>Test</h1>')
